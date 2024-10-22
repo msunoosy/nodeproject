fe17641408ca290bfd8556600807a5fdba132520
zamzamy Mohamed sunoosy <m.sunoos@gmail.com>
2024-10-22 19:18:26 +0400

    Used promises to call file systems

--- Backend/server.js
+++ Backend/server.js
@@ -1,24 +1,28 @@
+const fsPromises = require("fs").promises;
+const path = require("path");
 
-const fs = require('node:fs');
-const { start } = require('node:repl');
-const path=require('path');
+const fileOps = async () => {
+  try {
+    const data = await fsPromises.readFile("./start.txt", "utf8");
+    console.log(data);
+    await fsPromises.writeFile(
+      path.join(__dirname, "testwring.txt"),
+      "writing is working here"
+    );
+    console.log("writing completed");
+    await fsPromises.appendFile(
+      path.join(__dirname, "testwring.txt"),
+      "\n\nupdating is working"
+    );
+    console.log("append completed");
+    await fsPromises.rename(
+      path.join(__dirname, "testwring.txt"),
+      path.join(__dirname, "testingrenamed.txt")
+    );
+    console.log("rename completed");
+  } catch (err) {
+    console.error(err);
+  }
+};
 
-fs.readFile('./start.txt','utf8',(err,data)=>{
-    if(err)throw(err)
-        console.log(data)
-})
-
-fs.writeFile(path.join(__dirname,'testwring.txt'),"writing is working here",(err)=>{
-    if(err)throw(err)
-        console.log('writing completed')
-fs.appendFile(path.join(__dirname,'testwring.txt'),'\n\nupdating is working',(err)=>{
-    if(err)throw(err)
-        console.log('append completed')
-
- fs.rename(path.join(__dirname,'testwring.txt'),path.join(__dirname,'testingrenamed.txt'),(err)=>{
-    if(err)throw(err)
-        console.log('rename completed')
- })
-    
-})
-})
+fileOps();
