4d0af899c8892a21bf622b6a71b32cc5756a240e
zamzamy Mohamed sunoosy <m.sunoos@gmail.com>
2024-10-22 18:48:36 +0400

    Node File system

--- Backend/server.js
+++ Backend/server.js
@@ -1,1 +1,24 @@
-console.log('node server runs')
+
+const fs = require('node:fs');
+const { start } = require('node:repl');
+const path=require('path');
+
+fs.readFile('./start.txt','utf8',(err,data)=>{
+    if(err)throw(err)
+        console.log(data)
+})
+
+fs.writeFile(path.join(__dirname,'testwring.txt'),"writing is working here",(err)=>{
+    if(err)throw(err)
+        console.log('writing completed')
+fs.appendFile(path.join(__dirname,'testwring.txt'),'\n\nupdating is working',(err)=>{
+    if(err)throw(err)
+        console.log('append completed')
+
+ fs.rename(path.join(__dirname,'testwring.txt'),path.join(__dirname,'testingrenamed.txt'),(err)=>{
+    if(err)throw(err)
+        console.log('rename completed')
+ })
+    
+})
+})
